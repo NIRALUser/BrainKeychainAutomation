49be662620d613e54a0171b3e418c0c4466a7a76
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-30 11:49:36 -0500

    Add automation to work with every directory

--- keyChainNameTagCreator.bash
+++ keyChainNameTagCreator.bash
@@ -1,15 +1,13 @@
 #!/bin/bash
-cd /Users/christiannell/desktop/research/NIRAL/fullAutomation
 saveDir="Nametags"
 openSCAD_file="keyChainTitle.scad"
-nameTagInputDir="Priority4"
+nameTagInputDir="Keychains"
 subStr=""
 declare -a doneTags
 mkdir $saveDir
 cd $nameTagInputDir
-for FILE in *.vtk; 
+for FILE in *.stl; 
 	do (
-		echo 
 		if [ "${FILE:0:7}" == "stx_neo" ]; then 
 			pat="([^neo]*)-([^-]*)"
 			[[ $FILE =~ $pat ]]	
@@ -18,6 +16,9 @@ for FILE in *.vtk;
 			echo $subStr
 		elif [ "${FILE:0:11}" == "stx_noscale" ]; then
 			subStr="${FILE:12:6}"
+			echo $subStr
+		elif [ "${FILE:0:15}" == "stx_stx_noscale" ]; then
+			subStr="${FILE:16:6}"
 			echo $subStr
 		elif [ "${FILE:0:4}" == "stx_" ]; then
 			pat="([^_]*)-([^-]*)"
@@ -29,4 +30,3 @@ for FILE in *.vtk;
 		/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o "${saveDir}/${subStr}.stl" $openSCAD_file -D "input=\"$subStr\"" -D "length=12"
 		cd $nameTagInputDir
 	);done
-
